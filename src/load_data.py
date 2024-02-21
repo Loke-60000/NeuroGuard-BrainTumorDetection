aac9cd69cd1ee7f868cdae16e7c990ecc76f84c1
jimmy_code <jimmy.fernandez.devia@gmail.com>
2024-02-21 12:43:37 +0100

    create main.py

--- src/load_data.py
+++ src/load_data.py
@@ -3,34 +3,26 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
-def load_data(path):
-
-    path_yes = os.path.join(path, "yes")
-    path_no = os.path.join(path, "no")
-
+def load_data(path, target_size=(224, 224)):
+    """
+    Charge les images à partir du chemin spécifié et les redimensionne à la taille cible.
+    
+    :param path: Chemin vers le répertoire contenant les images.
+    :param target_size: Taille cible des images (par défaut : (224, 224) pour VGG16).
+    :return: Tableaux NumPy des images et des étiquettes correspondantes.
+    """
     images = []
     labels = []
 
-    for folder_path, label in [(path_yes, 1), (path_no, 0)]:
+    for folder_name in os.listdir(path):
+        folder_path = os.path.join(path, folder_name)
+        label = 1 if folder_name == "yes" else 0  # assigner 1 aux images dans le dossier "yes", sinon 0
         for img_name in os.listdir(folder_path):
             img_path = os.path.join(folder_path, img_name)
             img = cv2.imread(img_path)
-            
+            img = cv2.resize(img, target_size)  # redimensionner l'image à la taille cible
             images.append(img)
             labels.append(label)
 
-    return images, labels
+    return np.array(images), np.array(labels)
 
-# Example usage:
-train = "data/train"
-test = "data/test"
-val = "data/val"
-images, labels = load_data(train)
-
-load_data(train)
-plt.imshow(images[12])
-plt.title(f"label : {labels[12]}")
-
-
-print("Nombre total d'images chargées :", len(images))
-# print("Nombre total de labels chargés :", len(labels))
